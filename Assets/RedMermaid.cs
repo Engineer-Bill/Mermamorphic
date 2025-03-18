aec0fab56bec58f2609c8dff718df046c5a6a243
Samuel Cox <68568350+sunsetworld@users.noreply.github.com>
2025-03-18 11:24:58 +0000

    Level and player changes

---
+++ Assets/RedMermaid.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class RedMermaid : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnAbilityUse(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,24 +6,31 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
     Rigidbody2D _rb;
+    private Vector2 _movementVelocity;
+
+    private bool _canMove;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if (_canMove) _rb.MovePosition(_rb.position + _movementVelocity * (speed * Time.fixedDeltaTime));
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
         if (context.performed)
         {
-            Vector2 movement = context.ReadValue<Vector2>();
-            _rb.linearVelocity += new Vector2(movement.x * speed, movement.y * speed);
+            _movementVelocity = context.ReadValue<Vector2>();
+            _canMove = true;
+        }
+        else if (context.canceled)
+        {
+            _movementVelocity = Vector2.zero;
+            _canMove = false;
         }
     }
 }
