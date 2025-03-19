421cbce82daf4c19f717aae65304a214d7bac0f7
Samuel Cox <68568350+sunsetworld@users.noreply.github.com>
2025-03-19 21:25:25 +0000

    Minor adjustments

--- Assets/RedMermaid.cs
+++ Assets/RedMermaid.cs
@@ -3,6 +3,7 @@ using UnityEngine.InputSystem;
 
 public class RedMermaid : MonoBehaviour
 {
+    private Vector2 _aim;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,6 +14,13 @@ public class RedMermaid : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void GetAim(InputAction.CallbackContext context)
+    {
+        if (!context.performed) return;
+        _aim = context.ReadValue<Vector2>();
+        Debug.Log(_aim);
     }
 
     public void OnAbilityUse(InputAction.CallbackContext context)
