c16f79e7601cf733df1a2cc94253f7663b310cc7
Samuel Cox <68568350+sunsetworld@users.noreply.github.com>
2025-03-22 17:00:44 +0000

    Art files

--- Assets/RedMermaid.cs
+++ Assets/RedMermaid.cs
@@ -1,19 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class RedMermaid : MonoBehaviour
 {
     private Vector2 _aim;
+    public Transform headTransform;
+
+    private bool _canAttack;
+    public float attackLength = 5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (!_canAttack) return;
+        var hit = Physics2D.Raycast(headTransform.position, Vector2.right * attackLength);
+        if (hit.collider.CompareTag("BreakableWall"))
+        {
+            Destroy(hit.collider.gameObject);
+        }
     }
 
     public void GetAim(InputAction.CallbackContext context)
@@ -25,9 +35,14 @@ public class RedMermaid : MonoBehaviour
 
     public void OnAbilityUse(InputAction.CallbackContext context)
     {
-        if (context.performed)
-        {
-            
-        }
+        if (!context.performed) return;
+        _canAttack = true;
+        StartCoroutine(CancelAttack());
+    }
+
+    IEnumerator CancelAttack()
+    {
+        yield return new WaitForSeconds(0.5f);
+        _canAttack = false;
     }
 }
